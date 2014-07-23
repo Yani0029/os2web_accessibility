7cc02852b1530ddb4a473de82102605e8c319c3a
yani <yani@706drupal02.intern.syddjurs.dk>
2014-07-23 10:51:40 +0200

    Add css and other changes in handicap pages

--- os2web_accessibility.tpl.php
+++ os2web_accessibility.tpl.php
@@ -17,40 +17,50 @@
     <?php print $handicap['robots_meta']; ?>
     <?php print $handicap['favicon']; ?>
     <?php print $handicap['css']; ?>
-    <link type="text/css" rel="stylesheet" media="screen" href="itchefer.bellcom.dk/sites/all/themes/itchefer_theme/css/print.css">
+<link type="text/css" rel="stylesheet" media="screen" href="/<?php print drupal_get_path('module','os2web_accessibility').'/os2web_accessibility.css' ?>">
   </head>
-  <body style="font-size:13px; margin-bottom: 0px; max-height: 970px">
+  <body style="font-size:13px; margin-bottom: 0px; max-height: 970px; background-color:#f4eded;">
+  <div class="body-content-inner" style="max-width:1170px;background-color:#fff;">
     <?php if (!empty($handicap['message'])) {
       //print '<div class="print-message">'. $handicap['message'] .'</div><p />';
     } ?>
-
-    <div class="print-site_name" style="margin: 20px 20px; font-size: 24px;"><div class="print-logo" style="display: inline-block; margin-right: 10px;"><?php print $handicap['logo']; ?></div><?php print $handicap['site_name']; ?><hr></div>
-    <p />
-    <div class="print-breadcrumb" style="margin: 0 20px 0 40px;"><?php print $handicap['breadcrumb']; ?><hr class="print-hr" /></div>
+    <div class="print-site_name" style="margin: 20px 20px; font-size: 24px;">
+       <div class="print-logo" style="display: inline-block; margin-right: 10px;">
+           <?php print $handicap['logo']; ?>
+       </div><?php //print $handicap['site_name']; ?>
+       <div class="print-breadcrumb" style=""><?php print $handicap['breadcrumb']; ?><hr class="print-hr" />
+       </div>
+    </div>
 
     <div class="print-content" style='margin-bottom: 10px; max-height: 970px;'>
       <?php $nid = variable_get('os2web_accessibility_node_content');
             $arg = arg(1);
             $arg_0 = arg(0);
         if((isset($arg) && $nid != $arg) ||($arg_0 != "handicap" && !$arg)): ?>
-          <div><a href="/handicap" style="text-decoration: underline">Tilbage til Handicap forside</a></div>
+          <div style="margin-left: 40px;"><a href="/handicap" style="text-decoration: underline">Tilbage til Handicap forside</a></div>
         <?php endif; ?>
-      <div class="handicap-menu" style="float: left; width:200px; min-height: 200px;clear:both;">
-        <?php print $menu; ?>
+        <div class="handicap-menu" style="float: left; width:200px; min-height: 200px;clear:both;">
+          <?php print $menu; ?>
+        </div>
+        <div class="handicap-content" style="float:left; width: 600px; padding: 20px 20px;">
+          <?php print $handicap['content']; ?></div>
+        <div class="handicap-right" style="float: right; width: 300px; min-height: 300px;">
+          <?php print $right_blocks; ?></div>
       </div>
-      <div class="handicap-content" style="float:left; width: 600px;">
-        <?php print $handicap['content']; ?></div>
-      <div class="handicap-right" style="float: right; width: 300px; min-height: 300px;">
-        <?php print $right_blocks; ?></div>
-      </div>
-    </div>
-    <div class="print-foot" style = "width:100%; clear:both">
-      <hr class="" />
+
+    <hr class="print-hr">
+    <div class="print-foot" style = "width:100%; clear:both; padding:10px 40px;">
+      <?php $block = block_load('block','3');
+            $output = _block_get_renderable_array(_block_render_blocks(array($block)));
+            print drupal_render($output);
+      ?>
       <?php print $handicap['footer_message']; ?>
 
     </div>
     <div class="print-source_url"><?php //print $handicap['source_url']; ?></div>
     <div class="print-links"><?php //print $handicap['pfp_links']; ?></div>
     <?php print $handicap['footer_scripts']; ?>
+    <div style='clear:both;'></div>
+    </div>
   </body>
 </html>
